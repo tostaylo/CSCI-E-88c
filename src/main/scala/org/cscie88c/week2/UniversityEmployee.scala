5f61f3e3d52cfc43adb857bc3cc5697054938340
Torre <tostaylo@gmail.com>
2022-02-05 13:48:10 -0600

    week 2 - implemented employee class

--- src/main/scala/org/cscie88c/week2/UniversityEmployee.scala
+++ src/main/scala/org/cscie88c/week2/UniversityEmployee.scala
@@ -1,3 +1,7 @@
 package org.cscie88c.week2
 
-// write the class UniversityEmployee below
+class UniversityEmployee(val name: String, val email: String) {
+
+  def description(): String = s"Name: $name, Email: $email"
+
+}
--- src/test/scala/org/cscie88c/week2/UniversityEmployeeTest.scala
+++ src/test/scala/org/cscie88c/week2/UniversityEmployeeTest.scala
@@ -1,13 +1,23 @@
 package org.cscie88c.week2
 
-import org.cscie88c.testutils.{StandardTest}
-
-// write unit tests for University employee below
+import org.cscie88c.testutils.{ StandardTest }
 class UniversityEmployeeTest extends StandardTest {
   "UniversityEmployee" when {
+    val employee1 = new UniversityEmployee("Todd", "todd@toddmail.com")
+    val employee2 = new UniversityEmployee("Jane", "jane@janemail.com")
+
     "instantiated" should {
       "have a name property" in {
-        true shouldBe true
+        employee1.name shouldBe "Todd"
+        employee2.name shouldBe "Jane"
+      }
+      "have a email property" in {
+        employee1.email shouldBe "todd@toddmail.com"
+        employee2.email shouldBe "jane@janemail.com"
+      }
+      "have a description" in {
+        employee1.description() shouldBe "Name: Todd, Email: todd@toddmail.com"
+        employee2.description() shouldBe "Name: Jane, Email: jane@janemail.com"
       }
     }
   }
