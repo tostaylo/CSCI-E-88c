fe0e3048803a04b8a47a214dc00bb63c5c05ad92
Torre <tostaylo@gmail.com>
2022-02-06 13:31:06 -0600

    week 2 - admin and faculty classes

--- src/main/scala/org/cscie88c/week2/Faculty.scala
+++ src/main/scala/org/cscie88c/week2/Faculty.scala
@@ -1,3 +1,11 @@
 package org.cscie88c.week2
 
-// write code for class Faculty below
+class Faculty(
+    name: String,
+    email: String,
+    course: String
+  ) extends UniversityEmployee(name, email) {
+
+  override def description(): String =
+    s"Name: $name, Email: $email, Course: $course"
+}
--- src/test/scala/org/cscie88c/week2/FacultyTest.scala
+++ src/test/scala/org/cscie88c/week2/FacultyTest.scala
@@ -1,8 +1,26 @@
 package org.cscie88c.week2
 
-import org.cscie88c.testutils.{StandardTest}
+import _root_.org.cscie88c.testutils.StandardTest
 
-// write unit tests for Faculty below
 class FacultyTest extends StandardTest {
-  
+  "Faculty" when {
+    val faculty1 = new Faculty("Todd", "todd@toddmail.com", "History")
+    val faculty2 = new Faculty("Jane", "jane@janemail.com", "Math")
+
+    "instantiated" should {
+      "have a name property" in {
+        faculty1.name shouldBe "Todd"
+        faculty2.name shouldBe "Jane"
+      }
+      "have a email property" in {
+        faculty1.email shouldBe "todd@toddmail.com"
+        faculty2.email shouldBe "jane@janemail.com"
+      }
+      "have a description" in {
+        faculty1.description() shouldBe "Name: Todd, Email: todd@toddmail.com, Course: History"
+        faculty2.description() shouldBe "Name: Jane, Email: jane@janemail.com, Course: Math"
+      }
+    }
+  }
+
 }
