dc0e6c72ca96218e61626e0b4ec3394cb255a420
Torre <tostaylo@gmail.com>
2022-02-08 23:22:35 -0600

    Subject tests passing

--- src/test/scala/org/cscie88c/week2/SubjectTest.scala
+++ src/test/scala/org/cscie88c/week2/SubjectTest.scala
@@ -1,9 +1,19 @@
 package org.cscie88c.week2
 
-import org.cscie88c.testutils.{StandardTest}
-
-// write unit tests for Subject below
+import _root_.org.cscie88c.testutils.StandardTest
 
 class SubjectTest extends StandardTest {
-  
+  "Subject" when {
+    "companion class" should {
+      "get STEM subjects" in {
+        Subject.stemSubjects should equal(
+          List(
+            Subject(1, "Physics", true),
+            Subject(2, "Chemistry", true),
+            Subject(3, "Math", true)
+          )
+        )
+      }
+    }
+  }
 }
