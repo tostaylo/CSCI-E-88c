46e183e836a935231b4ef7aed137cb22f17336b9
Torre <tostaylo@gmail.com>
2022-02-22 17:24:02 -0600

    week 4 - applyNTimes and myPositivePower

--- src/main/scala/org/cscie88c/week4/FunctionUtils.scala
+++ src/main/scala/org/cscie88c/week4/FunctionUtils.scala
@@ -1,11 +1,16 @@
 package org.cscie88c.week4
 
 object FunctionUtils {
-  
-  // complete the implementation of the higher order functions below
-  def applyNtimes(n: Int)(x: Int)(f: Int => Int): Int = ???
 
-  def myPositivePower(x: Int, n: Int): Int = ???
+  def applyNtimes(n: Int)(x: Int)(f: Int => Int): Int = {
+    val list = List.fill(n)(f)
+    list.foldLeft(x)((acc, next) => next(acc))
+  }
+
+  def myPositivePower(x: Int, n: Int): Int = {
+    def square(base: Int): Int = base * x;
+    applyNtimes(n - 1)(x)(square)
+  }
 
   def deferredExecutor(name: String)(f: Int => Int): Int => Int = ???
 }
--- src/test/scala/org/cscie88c/week4/FunctionUtilsTest.scala
+++ src/test/scala/org/cscie88c/week4/FunctionUtilsTest.scala
@@ -1,18 +1,27 @@
 package org.cscie88c.week4
 
-import org.cscie88c.testutils.{StandardTest}
+import org.cscie88c.testutils.{ StandardTest }
 
 class FunctionUtilsTest extends StandardTest {
 
-   "FunctionUtils" when {
+  "FunctionUtils" when {
     "calling applyNtimes" should {
-      "return the correct value" in {
-        // write unit test here
+      "return the correct value with add5" in {
+        def add5(x: Int) = x + 5
+        FunctionUtils.applyNtimes(3)(0)(add5) should be(15)
+        FunctionUtils.applyNtimes(3)(1)(add5) should be(16)
+        FunctionUtils.applyNtimes(2)(10)(add5) should be(20)
+        FunctionUtils.applyNtimes(1)(1)(add5) should be(6)
       }
-    
+
+      "return the correct value with power" in {
+        FunctionUtils.myPositivePower(2, 2) should be(4)
+        FunctionUtils.myPositivePower(2, 4) should be(16)
+        FunctionUtils.myPositivePower(3, 2) should be(9)
+        FunctionUtils.myPositivePower(3, 3) should be(27)
+      }
     }
 
-    // write unit tests for other functions here
   }
- 
+
 }
