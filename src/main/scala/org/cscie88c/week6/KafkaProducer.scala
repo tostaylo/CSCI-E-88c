d25737b6df93f544bb8c46891be59e95084b60c0
Torre <tostaylo@gmail.com>
2022-03-05 18:41:41 -0600

    week 6 - Kafka Producer

--- src/main/scala/org/cscie88c/week6/KafkaProducer.scala
+++ src/main/scala/org/cscie88c/week6/KafkaProducer.scala
@@ -1,17 +1,23 @@
 package org.cscie88c.week6
 
-// define trait KafkaProducer below
+trait KafkaProducer {
+  def send(message: String): String
+}
 
-// define the case class SimpleKafkaProducer below
+case class SimpleKafkaProducer(topic: String) extends KafkaProducer {
 
-// define the companion object SimpleKafkaProducer below
+  def send(message: String): String = s"Topic:${topic} Message:${message}"
+}
+
+object SimpleKafkaProducer {
+  implicit val defaultKafkaProducer: SimpleKafkaProducer =
+    new SimpleKafkaProducer("default-topic")
+}
 
 // uncomment the lines below once you have implemented KafkaProducer and SimpleKafkaProducer
-/*
+
 object KafkaClient {
   // sends a status message to kafka
-  def sendStatusEvent(status: String)(implicit kafkaProducer: KafkaProducer) = {
+  def sendStatusEvent(status: String)(implicit kafkaProducer: KafkaProducer) =
     kafkaProducer.send(status) // use the implicit KafkaProducer provided
-  }  
 }
-*/
--- src/test/scala/org/cscie88c/week6/KafkaProducerTest.scala
+++ src/test/scala/org/cscie88c/week6/KafkaProducerTest.scala
@@ -5,9 +5,10 @@ import org.cscie88c.testutils.{ StandardTest }
 class KafkaProducerTest extends StandardTest {
   "KafkaClient" should {
     "send a message to the default topic" in {
-      // add your unit tests for KafkaClient.send here
-      // uncomment line below to provide the default KafkaProducer instance to call KafkaClient.sendStatusEvent
-      // import SimpleKafkaProducer._
+      import SimpleKafkaProducer._
+      KafkaClient.sendStatusEvent("the message") should be(
+        "Topic:default-topic Message:the message"
+      )
     }
   }
 }
