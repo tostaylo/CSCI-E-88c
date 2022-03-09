7936edd68a1fd14c9786a33b37b13586a3cd6d84
Torre <tostaylo@gmail.com>
2022-03-08 22:24:26 -0600

    week 6 - addable type class with ints and bools

--- src/main/scala/org/cscie88c/week6/AddableTypeclass.scala
+++ src/main/scala/org/cscie88c/week6/AddableTypeclass.scala
@@ -1,18 +1,30 @@
 package org.cscie88c.week6
 
-// Write a generic trait AddableTypeclass parameterized by type A 
+// Write a generic trait AddableTypeclass parameterized by type A
 trait AddableTypeclass[A] {
-  // add trait methods below
+  def addTwoValues(a: A, b: A): A
 }
 
 object AddableTypeclass {
-  
-  implicit val intAddableTypeclass: AddableTypeclass[Int] = ???
 
-  implicit val boolAddableTypeclass: AddableTypeclass[Boolean] = ???
+  implicit val intAddableTypeclass: AddableTypeclass[Int] =
+    new AddableTypeclass[Int] {
+
+      def addTwoValues(a: Int, b: Int): Int = a + b
+    }
+
+  implicit val boolAddableTypeclass: AddableTypeclass[Boolean] =
+    new AddableTypeclass[Boolean] {
+
+      def addTwoValues(a: Boolean, b: Boolean): Boolean = a || b
+    }
 
 }
 
 object AddableAggregator {
-  def sumWithAddable[A](list: List[A])(implicit addable: AddableTypeclass[A]): A = ???
+  def sumWithAddable[A](
+      list: List[A]
+    )(implicit
+      addable: AddableTypeclass[A]
+    ): A = list.reduceLeft((a, b) => addable.addTwoValues(a, b))
 }
--- src/test/scala/org/cscie88c/week6/AddableTypeclassTest.scala
+++ src/test/scala/org/cscie88c/week6/AddableTypeclassTest.scala
@@ -3,13 +3,30 @@ package org.cscie88c.week6
 import org.cscie88c.testutils.{ StandardTest }
 
 class AddableTypeclassTest extends StandardTest {
-  
+
   "AddableAggregator" should {
     "sum a list of integers" in {
-      // add your unit tests here
+      AddableAggregator.sumWithAddable(List(1, 2, 3, 4, 5))(
+        AddableTypeclass.intAddableTypeclass
+      ) should be(15)
+
     }
     "sum a list of booleans" in {
-      // add your unit tests here
+      AddableAggregator.sumWithAddable(List(true, true))(
+        AddableTypeclass.boolAddableTypeclass
+      ) should be(true)
+
+      AddableAggregator.sumWithAddable(List(true, false))(
+        AddableTypeclass.boolAddableTypeclass
+      ) should be(true)
+
+      AddableAggregator.sumWithAddable(List(false, true))(
+        AddableTypeclass.boolAddableTypeclass
+      ) should be(true)
+
+      AddableAggregator.sumWithAddable(List(false, false))(
+        AddableTypeclass.boolAddableTypeclass
+      ) should be(false)
     }
     "sum a list of employees" in {
       // add your unit tests here
