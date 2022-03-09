693d38cf6313c7bb26d3c09d3eaa19e065734459
Torre <tostaylo@gmail.com>
2022-03-08 21:43:26 -0600

    week 6 - Employee

--- src/main/scala/org/cscie88c/week6/Employee.scala
+++ src/main/scala/org/cscie88c/week6/Employee.scala
@@ -1,13 +1,26 @@
 package org.cscie88c.week6
 
-final case class Employee(name: String, age: Int, salary: Int)
+final case class Employee(
+    name: String,
+    age: Int,
+    salary: Int
+  )
 
 object Employee {
 
-  implicit val employeeOrdering: Ordering[Employee] = ???
+  implicit val employeeOrdering: Ordering[Employee] = new Ordering[Employee] {
 
-  def defaultSortEmployees(employees: List[Employee]): List[Employee] = ???
+    def compare(x: Employee, y: Employee): Int = x.name compare y.name
+  }
 
-  def sortEmployeesBySalary(employees: List[Employee]): List[Employee] = ???
-  
+  def defaultSortEmployees(employees: List[Employee]): List[Employee] =
+    employees.sorted
+
+  def sortEmployeesBySalary(employees: List[Employee]): List[Employee] = {
+    implicit val employeeOrdering: Ordering[Employee] =
+      Ordering.by[Employee, Int](_.salary)
+
+    employees.sorted
+  }
+
 }
--- src/test/scala/org/cscie88c/week6/EmployeeTest.scala
+++ src/test/scala/org/cscie88c/week6/EmployeeTest.scala
@@ -4,13 +4,37 @@ import org.cscie88c.testutils.{ StandardTest }
 
 class EmployeeTest extends StandardTest {
   "Employee" should {
- 
+    val employees = List(
+      Employee("Frank", 60, 200),
+      Employee("Dee", 36, 250),
+      Employee("Mac", 38, 210),
+      Employee("Dennis", 40, 201),
+      Employee("Charlie", 40, 190)
+    )
+
     "have a default sort order" in {
-      // write unit tests here
+
+      Employee.defaultSortEmployees(employees) should be(
+        List(
+          Employee("Charlie", 40, 190),
+          Employee("Dee", 36, 250),
+          Employee("Dennis", 40, 201),
+          Employee("Frank", 60, 200),
+          Employee("Mac", 38, 210)
+        )
+      )
     }
 
     "sort employees by salary" in {
-      // write unit tests here
+      Employee.sortEmployeesBySalary(employees) should be(
+        List(
+          Employee("Charlie", 40, 190),
+          Employee("Frank", 60, 200),
+          Employee("Dennis", 40, 201),
+          Employee("Mac", 38, 210),
+          Employee("Dee", 36, 250)
+        )
+      )
     }
   }
 }
