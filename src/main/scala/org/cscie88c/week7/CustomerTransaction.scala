14dfd5eea5fa1f822968af3209890273decf2bc6
Torre <tostaylo@gmail.com>
2022-03-13 15:18:46 -0500

    week 7 - clean dirty customer transactions

--- src/main/scala/org/cscie88c/week7/CustomerTransaction.scala
+++ src/main/scala/org/cscie88c/week7/CustomerTransaction.scala
@@ -1,25 +1,40 @@
 package org.cscie88c.week7
 
 import scala.io.Source
-import scala.util.{Try, Success, Failure}
+import scala.util.{ Failure, Success, Try }
 
 final case class CustomerTransaction(
-  customerId: String,
-  transactionDate: String,
-  transactionAmount: Double
-)
+    customerId: String,
+    transactionDate: String,
+    transactionAmount: Double
+  )
 
 object CustomerTransaction {
-  def apply(csvString: String): Option[CustomerTransaction]= {
-     try {
+  def apply(csvString: String): Option[CustomerTransaction] =
+    try {
       val csvToList = csvString.split(",")
-      val isDouble = csvToList(2).toDouble
-      if(csvToList.length != 3) throw new Exception()
-      
-      Some(CustomerTransaction(csvToList(0), csvToList(1), csvToList(2).toDouble))
+      val toDouble = csvToList(2).toDouble
 
-    } catch {
-     case e: Exception => None
-   }
-  }
+      if (csvToList.length != 3) throw new Exception()
+
+      Some(
+        CustomerTransaction(csvToList(0), csvToList(1), toDouble)
+      )
+
+    }
+    catch {
+      case e: Exception => None
+    }
+
+  def readFromCSVFile(fileName: String): List[CustomerTransaction] =
+    Source
+      .fromFile(fileName)
+      .getLines()
+      .collect { line =>
+        CustomerTransaction.apply(line) match {
+          case Some(customerTransaction) => customerTransaction
+        }
+      }
+      .toList
+
 }
