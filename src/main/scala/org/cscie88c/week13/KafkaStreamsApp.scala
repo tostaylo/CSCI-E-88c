e6779f0041e76d280047f8ca849d0e78ceaefef8
Torre <tostaylo@gmail.com>
2022-04-28 21:57:58 -0500

    finding similar genre songs

--- src/main/scala/org/cscie88c/week13/KafkaStreamsApp.scala
+++ src/main/scala/org/cscie88c/week13/KafkaStreamsApp.scala
@@ -16,15 +16,25 @@ object KafkaStreamsApp {
   def main(args: Array[String]): Unit = {
     import Serdes._
 
+    val inputSong = "7mitXLIMCflkhZiD34uEQI"
     val songs = Song
       .readFromCSVFile(
         "src/main/resources/data/spotify_songs.csv"
       )
 
+    val foundSong = songs.filter(_.id == inputSong)(0)
+
+    val sameGenreSongs =
+      songs.filter(song =>
+        song.playlistGenre == foundSong.playlistGenre && song.playlistSubGenre == foundSong.playlistSubGenre
+      )
+
+    // the key to this application will be building a  graph which will have the closest songs in similarity in the neighboring nodes.  But also a binary search tree for matching the song quickly to begin with. Will bloom filter help?
+
     // 1. define kafka streams properties, usually from a config file
     val props: Properties = {
       val p = new Properties()
-      p.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application")
+      p.put(StreamsConfig.APPLICATION_ID_CONFIG, "song-recommender-application")
       p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
       p
     }
@@ -32,7 +42,7 @@ object KafkaStreamsApp {
     // 2. create KStreams DSL instance
     val builder: StreamsBuilder = new StreamsBuilder
     val textLines: KStream[String, String] =
-      builder.stream[String, String]("TextLinesTopic")
+      builder.stream[String, String]("SongTopic")
 
     // 3. transform the data
     // val wordCounts: KTable[String, Long] = textLines
@@ -53,7 +63,11 @@ object KafkaStreamsApp {
     //   .map((word, count) => (word, s"$word: $count"))
     //   .to("WordsWithCountsTopic")
 
-    songs.foreach(println)
+    sameGenreSongs.foreach(song =>
+      println(
+        s"Artist: ${song.artist} | Song: ${song.name} | Genre: ${song.playlistGenre} | Sub-genre: ${song.playlistSubGenre}"
+      )
+    )
 
     /** write a `String` to the `filename`.
       */
@@ -63,7 +77,7 @@ object KafkaStreamsApp {
       bw.write(s)
       bw.close()
     }
-    print(System.getProperty("user.dir"))
+
     writeFile("src/main/resources/data/myfile.txt", "bye bye")
 
     // 5. start the streams application
@@ -82,11 +96,29 @@ object KafkaStreamsApp {
 
 import scala.io.Source
 import scala.util.{ Failure, Success, Try }
-
 final case class Song(
     id: String,
     name: String,
-    artist: String
+    artist: String,
+    popularity: String,
+    albumId: String,
+    albumName: String,
+    albumReleaseDate: String,
+    playlistName: String,
+    playlistId: String,
+    playlistGenre: String,
+    playlistSubGenre: String,
+    danceability: String,
+    energy: String,
+    key: String,
+    loudness: String,
+    mode: String,
+    acousticness: String,
+    instrumentalness: String,
+    liveness: String,
+    valence: String,
+    tempo: String,
+    duration: String
   )
 
 object Song {
@@ -95,7 +127,30 @@ object Song {
       val csvToList = csvString.split(",")
 
       Some(
-        Song(csvToList(0), csvToList(1), csvToList(2))
+        Song(
+          csvToList(0),
+          csvToList(1),
+          csvToList(2),
+          csvToList(3),
+          csvToList(4),
+          csvToList(5),
+          csvToList(6),
+          csvToList(7),
+          csvToList(8),
+          csvToList(9),
+          csvToList(10),
+          csvToList(11),
+          csvToList(12),
+          csvToList(13),
+          csvToList(14),
+          csvToList(15),
+          csvToList(16),
+          csvToList(17),
+          csvToList(18),
+          csvToList(19),
+          csvToList(20),
+          csvToList(21)
+        )
       )
 
     }
