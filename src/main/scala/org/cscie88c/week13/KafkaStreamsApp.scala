b2314f5a701d4469f1bd427b3fd7166348be648d
Torre <tostaylo@gmail.com>
2022-04-28 21:02:38 -0500

    songs being printed and topics being input and output

--- src/main/scala/org/cscie88c/week13/KafkaStreamsApp.scala
+++ src/main/scala/org/cscie88c/week13/KafkaStreamsApp.scala
@@ -8,12 +8,18 @@ import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala._
 import org.apache.kafka.streams.scala.kstream._
 import org.apache.kafka.streams.{ KafkaStreams, StreamsConfig }
+import java.io._
 
 // run with: sbt "runMain org.cscie88c.week13.KafkaStreamsApp"
 object KafkaStreamsApp {
 
   def main(args: Array[String]): Unit = {
     import Serdes._
+
+    val songs = Song
+      .readFromCSVFile(
+        "src/main/resources/data/spotify_songs.csv"
+      )
 
     // 1. define kafka streams properties, usually from a config file
     val props: Properties = {
@@ -29,27 +35,83 @@ object KafkaStreamsApp {
       builder.stream[String, String]("TextLinesTopic")
 
     // 3. transform the data
-    val wordCounts: KTable[String, Long] = textLines
-      .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
-      .groupBy((_, word) => word)
-      .count()(Materialized.as("counts-store"))
+    // val wordCounts: KTable[String, Long] = textLines
+    //   .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
+    //   .groupBy((_, word) => word)
+    //   .count()(Materialized.as("counts-store"))
+
+    // val wordCounts: KTable[String, Long] = textLines
+    //   .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
+    //   .groupBy((_, word) => word)
+    //   .count()(Materialized.as("counts-store"))
 
     // 4. write the results to a topic or other persistent store
-    wordCounts
-      .toStream
-      // .peek((k,t) => println(s"stream element: $k: $t")) // to print items in stream
-      .filter((_, count) => count > 5)
-      .map((word, count) => (word, s"$word: $count"))
-      .to("WordsWithCountsTopic")
+    // wordCounts
+    //   .toStream
+    //   // .peek((k,t) => println(s"stream element: $k: $t")) // to print items in stream
+    //   .filter((_, count) => count > 5)
+    //   .map((word, count) => (word, s"$word: $count"))
+    //   .to("WordsWithCountsTopic")
+
+    songs.foreach(println)
+
+    /** write a `String` to the `filename`.
+      */
+    def writeFile(filename: String, s: String): Unit = {
+      val file = new File(filename)
+      val bw = new BufferedWriter(new FileWriter(file))
+      bw.write(s)
+      bw.close()
+    }
+    print(System.getProperty("user.dir"))
+    writeFile("src/main/resources/data/myfile.txt", "bye bye")
 
     // 5. start the streams application
     val streams: KafkaStreams = new KafkaStreams(builder.build(), props)
     streams.start()
 
     // 6. attach shutdown handler to catch control-c
-    sys.ShutdownHookThread {
-      streams.close(Duration.ofSeconds(10))
-    }
+    // sys.ShutdownHookThread {
+    //   streams.close(Duration.ofSeconds(10))
+    // }
+
+    streams.close(Duration.ofSeconds(10))
   }
+
+}
+
+import scala.io.Source
+import scala.util.{ Failure, Success, Try }
+
+final case class Song(
+    id: String,
+    name: String,
+    artist: String
+  )
+
+object Song {
+  def apply(csvString: String): Option[Song] =
+    try {
+      val csvToList = csvString.split(",")
+
+      Some(
+        Song(csvToList(0), csvToList(1), csvToList(2))
+      )
+
+    }
+    catch {
+      case e: Exception => None
+    }
+
+  def readFromCSVFile(fileName: String): List[Song] =
+    Source
+      .fromFile(fileName)
+      .getLines()
+      .collect { line =>
+        Song.apply(line) match {
+          case Some(song) => song
+        }
+      }
+      .toList
 
 }
